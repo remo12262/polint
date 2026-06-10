a3b4ef1990b9be5bb8c2babc210982aaf536f872
Quantum Horizon <54858877+remo12262@users.noreply.github.com>
2026-06-10 15:38:01 +0200

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>)
+import LegalGate from './LegalGate.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <LegalGate appName="POLINT">
+      <App />
+    </LegalGate>
+  </React.StrictMode>
+)
